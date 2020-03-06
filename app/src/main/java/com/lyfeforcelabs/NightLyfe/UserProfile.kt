e03a7ee258c39f9523396c0666fac8a15702b7ff
Timothy Richardson <timothy.richardson01@gmail.com>
2020-03-06 13:51:39 +1100

    Added boilerplate code that is responsible for processing user profile data.

--- app/src/main/java/com/lyfeforcelabs/NightLyfe/UserProfile.kt
+++ app/src/main/java/com/lyfeforcelabs/NightLyfe/UserProfile.kt
@@ -2,11 +2,42 @@ package com.lyfeforcelabs.NightLyfe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class UserProfile : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "User Profile"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+    }
+
+    // Basic I/O function to read from the firebase database.
+    fun basicReadWrite() {
+        // Write to the database.
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Method calls the initial information and updates it when required.
+                val value = dataSnapshot.getValue(String::class.java)
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read values from the database.
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
 }
